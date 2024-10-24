83a0c97c624d5795b69e3ceda8143e0ff4d6aa44
bigd <bogdan.istamilov@bk.ru>
2024-10-24 23:31:39 +0300

    hello

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,42 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+import (
+	"fmt"
+)
+
+func removeDuplicates(inputStream chan string, outputStream chan string) {
+	/*
+
+		if temp != <-inputStream {
+			temp = <-inputStream
+			outputStream <- temp
+		}
+	*/
+	var temp string
+	for v := range inputStream {
+		if temp != v {
+			temp = v
+			outputStream <- temp
+		}
+	}
+	close(outputStream)
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+
+	channel := make(chan string, 10)
+	channel1 := make(chan string, 10)
+
+	for _, r := range "1222345567" {
+		channel <- string(r)
+	}
+
+	close(channel)
+
+	go removeDuplicates(channel, channel1)
+
+	for v := range channel1 {
+		fmt.Println(v)
+	}
+
 }
